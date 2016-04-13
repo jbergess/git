e4c27869a495b6f98b3cce2bc48676ce6f820e88
Edwin Calderón <edca81@gmail.com>
2016-04-13 01:33:27 -0600

    Se agregaron controles de elasticidad funcionales. Controles de masa no funcionan. Comentarios en archivos de javascript.

--- proyecto final/proyecto-final/proyecto-final/js/fisik.js
+++ proyecto final/proyecto-final/proyecto-final/js/fisik.js
@@ -1,4 +1,10 @@
-// Matter.js module aliases
+/*
+ * fisik.js
+ * Archivo para pruebas con matter.js
+ * Creación inicial del motor 2D y objetos
+ */
+
+// Alias de los módulos
 var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
@@ -6,65 +12,54 @@ var Engine = Matter.Engine,
     MouseConstraint = Matter.MouseConstraint,
     Constraint = Matter.Constraint;
 
-// create a Matter.js engine
+// creación del motor
 var engine = Engine.create(document.getElementById("engine"));
 
-// set world gravity and timescale
-//engine.world.gravity.y = 1;
-//engine.timing.timeScale = 1;
-
-// create a box object
+// caja izquierda
 var box = Bodies.rectangle(100, 200, 50, 50);
-Body.setMass(box,5);
+//Body.setMass(box,5);
 
-// create a box object
+// caja libre
 var box2 = Bodies.rectangle(400, 400, 50, 50);
 Body.setMass(box,5);
 
-// create a ball object
+// esfera izquierda
 var ball = Bodies.circle(700, 200, 25, 50);
-Body.setMass(ball,20);
+//Body.setMass(ball,20);
 
-// create a ball object
+// esfera libre
 var ball2 = Bodies.circle(700, 200, 25, 50);
 Body.setMass(ball,20);
 
-// create floor and ceiling
+// techo y piso estáticos
 var ceiling = Bodies.rectangle(400, 10, 800, 10, { isStatic: true });
 var ground = Bodies.rectangle(400, 500, 800, 10, { isStatic: true });
 var left_wall = Bodies.rectangle(0, 10, 10, 990, { isStatic: true });
 var right_wall = Bodies.rectangle(800, 10, 10, 990, { isStatic: true });
-//add spring constraints
-var valuestiff=document.getElementById("stiffness").innerHTML;
- //println(valuestiff)
+
+// agrega resortes
+var valuestiffrightLeft=document.getElementById("stiffnessLeft").innerHTML;
+var valuestiffLeft=document.getElementById("stiffnessRight").innerHTML;
 var spring1 = Constraint.create({
 	pointA : {x:100, y:0},
-	bodyB : box, 
-	//stiffness: valuestiff
-	//stiffness: 0.1
-
+	bodyB : box,
 });
+
 spring1.lenght = 500;
 
 
 var spring2 = Constraint.create({
 	pointA : {x:700, y:0},
 	bodyB : ball,
-	stiffness: 0.1
 });
 spring2.lenght = 600;
 
-// Vector test
-//var vector1 = {x:5, y:5};
-//var vector2 = {x:4, y:8};
-
-
-// add a mouse controlled constraint for mouse input events
+// habilita interacción con mouse o touchscreen
 mouseConstraint = MouseConstraint.create(engine);
 
 
-// add all of the bodies to the world
+// agrega objetos al mundo
 World.add(engine.world, [box, box2, ball, ball2, ceiling, ground,left_wall,right_wall, mouseConstraint, spring1, spring2]);
 
-// run the engine
+// Inicia el motor
 Engine.run(engine);
--- proyecto final/proyecto-final/proyecto-final/js/main.js
+++ proyecto final/proyecto-final/proyecto-final/js/main.js
@@ -1,10 +1,42 @@
+/**
+ * main.js
+ * Archivo principal de javascript
+ * Actua como controlador entre intefaz y objetos de matter.js
+ */
+
+/* Función para control de la gravedad */
 function showGravityValue(newValue){
 	document.getElementById("range").innerHTML=newValue;
 	engine.world.gravity.y = newValue;	
 }
 
-function showStiffnessValue(newValue){	
-	/**	aqui le meti la variable que viene de el control , pero no se bien a cual variable de engine.world deberia controlar...**/
- 	document.getElementById("stiffness").innerHTML=newValue;
+/* Función para control del tiempo */
+function showTimeScaleValue(newValue){	
+ 	document.getElementById("timescale").innerHTML=newValue;
+	engine.timing.timeScale = newValue;	
+}
+
+/* Función para control de la constante de elasticidad del resorte izquierdo */
+function showStiffnessValueLeft(newValue){	
+ 	document.getElementById("stiffnessLeft").innerHTML=newValue;
 	spring1.stiffness = newValue;	
+}
+
+/* Función para control de la constante de elasticidad del resorte derecho */
+function showStiffnessValueRight(newValue){	
+ 	document.getElementById("stiffnessRight").innerHTML=newValue;
+	spring2.stiffness = newValue;	
+}
+
+/* Función para control de la masa de la caja */
+function showMassValueBox(newValue){	
+ 	document.getElementById("massBox").innerHTML=newValue;
+	//Body.setMass(box,newValue);
+	box.mass = newValue;	
+}
+
+/* Función para control de la masa del círculo */
+function showMassValueCircle(newValue){	
+ 	document.getElementById("massBall").innerHTML=newValue;
+	Body.setMass(ball,newValue);
 }
